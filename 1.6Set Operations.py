4953142aeb69f25f8df2b78b050cf9b8e890cd50
ALBIN K RAJAN <90998736+albinkrajan@users.noreply.github.com>
2021-10-14 12:40:25 +0530

    Add files via upload

---
+++ 1.6Set Operations.py
@@ -0,0 +1,9 @@
+s1 = {1, 2, 3, 4, 5, 'Albin', 'Pythonista'}
+s1.add('Machine Learning')
+print(s1)
+s1.update([10, 11, 12])
+print(s1)
+#removing elements
+s1.discard('Data Science')
+s1.remove(4)
+print(s1)
