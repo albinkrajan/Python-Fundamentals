bcb9de8e082d0afafaba3a7cafd8266a977a44ab
ALBIN K RAJAN <90998736+albinkrajan@users.noreply.github.com>
2021-10-14 12:39:26 +0530

    Add files via upload

---
+++ 1.3List Operations.py
@@ -0,0 +1,22 @@
+l1 = [1, 2, 3, 4, 5, 'Pythoninsta']
+l2 = ['a', 'b','c','d']
+#list  Slicing
+print(l1[:5])
+print(l1[5:])
+print(l1[0:5])
+print(l1[0:14:2])
+#updating list
+l2[2]=7
+print(l2)
+#appending value
+l2.append('a')
+print(l2)
+
+#inserting values in a specific position
+l2.insert(3, 'Albin')
+print(l2)
+#reverse the list
+l2.reverse()
+print(l2)
+
+
