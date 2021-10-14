70510719688712dac47ec4de24d2b46527eed66a
ALBIN K RAJAN <90998736+albinkrajan@users.noreply.github.com>
2021-10-14 12:51:08 +0530

    Add files via upload

---
+++ 7.3For Loop Sequence printing.py
@@ -0,0 +1,9 @@
+a = (1, 2, 3, 4, 5)
+b = [1, 2, 3, 4, 5]
+c = {1, 2, 3, 4, 5}
+for i in a:
+    print("Tuple elements are", i)
+for i in b:
+    print("List elements are", i)
+for i in c:
+    print("Set elements are", i)
