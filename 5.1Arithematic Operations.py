b1434937fdde22157062885c73929f31d692e6b1
ALBIN K RAJAN <90998736+albinkrajan@users.noreply.github.com>
2021-10-14 12:48:31 +0530

    Add files via upload

---
+++ 5.1Arithematic Operations.py
@@ -0,0 +1,32 @@
+#arithmetic operations on integer numbers
+i1 = int(input("Enter the integer number:"))
+i2 = int(input("Enter the 2nd integer number:"))
+print(i1+i2)
+print(i1-i2)
+print(i1*i2)
+print(i1/i2)
+print(i1%i2)
+print(i1**i2)
+print(i1//i2)
+
+#arithmetic operations on numbers with decimal points
+f1 = float(input("Enter the 1st complex number"))
+f2 = float(input("Enter the 1st complex number"))
+print(f1+f2)
+print(f1-f2)
+print(f1*f2)
+print(f1/f2)
+print(f1%f2)
+print(f1**f2)
+print(f1//f2)
+
+#arithmetic operations on complex numbers
+c1 = complex(input("Enter the 1st complex number"))
+c2 = complex(input("Enter the 2nd complex number"))
+print(c1+c2)
+print(c1-c2)
+print(c1*c2)
+print(c1/c2)
+print(c1**c2)
+#cannot perform modulus & floor division on complex numbers.
+
